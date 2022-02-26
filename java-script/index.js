63c2c5a08464aced55b7ae4cb4ecc48cdad6c933
Quvonchbek1998 <gayibovq@gmail.com>
2022-02-26 19:04:47 +0500

    end of HW

---
+++ java-script/index.js
@@ -0,0 +1,74 @@
+var j = 20;
+
+// j > 20
+// ? console.log('ha')
+// : console.log('yoq');
+// switch (j) {
+//     case 20:
+//         console.log('ha');
+//         break;
+//     case 25:
+//      console.log('yoq');
+//     break;
+//     case 15:
+//         console.log('yoq');
+//        break;
+//      default:
+//          console.log('aniq emas');
+//         break;
+// }
+// var n = 5;
+// if (n === 10 && n === 3 || n === 5) {
+//     console.log("ok");
+// }
+
+
+// for (var i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// var i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 5);
+
+// fuction tipes:
+// expression
+// function sum(params) {
+//     console.log(12345);
+// }
+// sum();
+
+// decloration
+// var fun2= function () {
+//     console.log(123);
+// }
+// fun2();
+
+// // arrow
+// var arrowfun = () =>{
+//     console.log(12232);
+// }
+// arrowfun()
+// end! 
+
+// function f(num) {
+//     console.log(num);
+// }
+// f()
+// f(2)
+
+function f(a, b, c,) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+f(2, { a: 5 }, 6);
+
+
+
